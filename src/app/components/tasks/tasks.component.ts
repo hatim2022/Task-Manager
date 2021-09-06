68791e284b7c030cf13e78f3c3f2f8d82c8b57fe
Your Name <you@example.com>
2021-09-06 11:07:20 -0400

    new commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+   tasks: Task[]=[];
+
+  constructor(private taskservice:TaskService) { }
+
+  ngOnInit(): void {
+  this.taskservice.getTask().subscribe((tasks) => this.tasks=tasks);
+  }
+
+  deleteTask(task:Task){
+    this.taskservice.deleteTask(task).
+    subscribe(() => this.tasks=this.tasks=this.tasks.filter(t=> t.id !== task.id));
+  }
+  toggleReminder(task:Task){
+    task.reminder= !task.reminder;
+    this.taskservice.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task){
+    this.taskservice.addTask(task).subscribe((task) => 
+    (this.tasks.push(task)));
+  }
+}
